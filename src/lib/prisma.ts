67ddbc71615c20b3f87d90f7b1eb1e0b61324fde
alexrotaru15 <alexrotaru1595@gmail.com>
2026-04-12 15:02:21 +0300

    prisma fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,21 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as {
 	prisma: PrismaClient | undefined;
+	pool: Pool | undefined;
 };
 
 function createPrismaClient() {
+	if (!globalForPrisma.pool) {
+		globalForPrisma.pool = new Pool({
+			connectionString: process.env.DATABASE_URL,
+		});
+	}
+	const adapter = new PrismaPg(globalForPrisma.pool);
 	return new PrismaClient({
+		adapter,
 		log:
 			process.env.NODE_ENV === "development"
 				? ["query", "error", "warn"]
