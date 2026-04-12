4cbe2689178ed340c9a249a338315155430e54cc
alexrotaru15 <alexrotaru1595@gmail.com>
2026-04-12 14:59:20 +0300

    fix remove adapter

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,20 +1,16 @@
 import { PrismaClient } from "@prisma/client";
-import { Pool } from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as {
 	prisma: PrismaClient | undefined;
-	pool: Pool | undefined;
 };
 
 function createPrismaClient() {
-	if (!globalForPrisma.pool) {
-		globalForPrisma.pool = new Pool({
-			connectionString: process.env.DATABASE_URL,
-		});
-	}
-	const adapter = new PrismaPg(globalForPrisma.pool);
-	return new PrismaClient({ adapter });
+	return new PrismaClient({
+		log:
+			process.env.NODE_ENV === "development"
+				? ["query", "error", "warn"]
+				: ["error"],
+	});
 }
 
 export const prisma = globalForPrisma.prisma ?? createPrismaClient();
